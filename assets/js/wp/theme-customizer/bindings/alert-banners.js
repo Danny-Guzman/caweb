a9740daea0424b07d97c27235123d32a4bcd30f4
Danny Guzman <guzmanjd86@gmail.com>
2020-07-28 15:05:49 -0700

    Alert Banners are now edited on the fly during live preview

--- assets/js/wp/theme-customizer/bindings/alert-banners.js
+++ assets/js/wp/theme-customizer/bindings/alert-banners.js
@@ -6,10 +6,62 @@ jQuery( document ).ready( function($) {
 		});
 	});
 
-	wp.customize( 'caweb_alert_banner_1', function( value ) {
-		value.bind( function( newval ) {
-			console.log( newval );
+	for( var a = 1; a < 20; a++){
+		wp.customize( 'caweb_alert_banner_' + a, function( value ) {
+			value.bind( function( newval ) {
+				var obj = JSON.parse( newval );
+				
+				
+				if( obj.alert ){
+					var alert = $( 'header .alert-' + obj.alert );
+					var alert_level = alert.find('.alert-level');
+					var alert_icon = alert_level.find('span');
+					var alert_link = alert.find('.alert-link');
+					var alert_msg = alert.find('.alert-text');
+
+					// Alert Status
+					if( obj.status ){
+						alert.removeClass('hidden');
+					}else{
+						alert.addClass('hidden');
+					}
+
+					// Alert Banner Color
+					alert.css('background-color', obj.banner_color );
+
+					// Alert Level ( Icon & Header )
+					alert_level.empty();
+					alert_icon.removeClass();
+					
+					if( obj.icon.trim() ){
+						alert_icon.addClass('ca-gov-icon-' + obj.icon );
+					}
+
+					alert_level.append(alert_icon);
+					alert_level.append(obj.header);
+
+					// Alert Message
+					alert_msg.html( obj.message );
+
+					// Read More Button
+					if( obj.read_more && obj.read_more_text.trim() && obj.read_more_url.trim() ){
+						alert_link.removeClass('hidden');
+					}else{
+						alert_link.addClass('hidden');
+					}
+
+					alert_link.html( obj.read_more_text );
+					alert_link.attr( 'href', obj.read_more_url );
+
+					if( obj.read_more_target ){
+						alert_link.attr( 'target', '_blank' );
+					}else{
+						alert_link.attr( 'target', '_self' );
+					}
+
+				}
+			});
 		});
-	});
+	}
 });
 
--- partials/content/alerts.php
+++ partials/content/alerts.php
@@ -5,54 +5,65 @@
  * @package CAWeb
  */
 
-$caweb_alerts = get_option( 'caweb_alerts', array() );
+caweb_display_alerts();
 
-if ( empty( $caweb_alerts ) ) {
-	return;
-}
 
-?>
-<!-- Alert Banners -->
-<?php
-foreach ( $caweb_alerts as $caweb_a => $caweb_data ) {
-	$caweb_status  = $caweb_data['status'];
-	$caweb_display = $caweb_data['page_display'];
+function caweb_display_alerts( ){
+	$alerts = get_option( 'caweb_alerts', array() );
+	$customizer_preview = is_customize_preview() ? ' hidden' : '';
+	
+	if ( empty( $alerts ) ) {
+		return;
+	}
+	
+	?>
+	<!-- Alert Banners -->
+	<?php
+	foreach ( $alerts as $a => $data ) {
+		$id = $a + 1;
+		$status  = $data['status'];
+		$display = $data['page_display'];
+		
+		/* If alert is active and should be displayed */
+		$active_alert = in_array( $status, array( 'active', 'on' ), true ) &&
+			( ( is_front_page() && 'home' === $display ) || ( 'all' === $display ) );
+	
+		if ( $active_alert || ! empty( $customizer_preview ) ) {
+			$alert_classes = "alert-$id";
+			$alert_classes .= ! $active_alert ? " $customizer_preview" : '' ;
+			$read_more = ! empty( $data['button'] ) && ! empty( $data['url'] ) && ! empty( $data['text'] );
 
-	/* If alert is active and should be displayed */
-	$caweb_active_alert = in_array( $caweb_status, array( 'active', 'on' ), true ) &&
-		( ( is_front_page() && 'home' === $caweb_display ) || ( 'all' === $caweb_display ) );
-
-	if ( $caweb_active_alert ) {
-		if ( isset( $_COOKIE[ "caweb-alert-id-$caweb_a" ] ) && sanitize_text_field( wp_unslash( $_COOKIE[ "caweb-alert-id-$caweb_a" ] ) ) ) {
-			?>
-			<div class="alert alert-dismissible alert-banner border-top border-dark alert-<?php print $caweb_a; ?>" style="background-color:<?php print esc_attr( $caweb_data['color'] ); ?>;">
-				<div class="container">
-					<button type="button" class="close caweb-alert-close" data-id="<?php print esc_attr( $caweb_a ); ?>" data-dismiss="alert" aria-label="Close">
-						<span aria-hidden="true">&times;</span>
-					</button>
-					<?php if ( ! empty( $caweb_data['header'] ) ) : ?>
-					<span class="alert-level">
-
-						<?php if ( ! empty( $caweb_data['icon'] ) ) : ?>
-							<span class="ca-gov-icon-<?php print esc_attr( $caweb_data['icon'] ); ?>" aria-hidden="true"></span>
-							<?php
-						endif;
-						print esc_html( $caweb_data['header'] );
-						?>
-					</span>
-					<?php endif; ?>
-					<span class="alert-text"><?php print wp_kses( wp_unslash( $caweb_data['message'] ), caweb_allowed_html( ) ); ?></span>
-						<?php
-						if ( ! empty( $caweb_data['button'] ) && ! empty( $caweb_data['url'] ) ) :
-							$caweb_url    = $caweb_data['url'];
-							$caweb_target = ! empty( $caweb_data['target'] ) ? sprintf( ' target="%1$s"', $caweb_data['target'] ) : '';
-							$caweb_text   = ! empty( $caweb_data['text'] ) ? $caweb_data['text'] : '';
+			if ( isset( $_COOKIE[ "caweb-alert-id-$id" ] ) && sanitize_text_field( wp_unslash( $_COOKIE[ "caweb-alert-id-$id" ] ) ) ) {
+				?>
+				<div class="alert alert-dismissible alert-banner border-top border-dark <?php print $alert_classes; ?>" style="background-color:<?php print esc_attr( $data['color'] ); ?>;">
+					<div class="container">
+						<button type="button" class="close caweb-alert-close" data-id="<?php print esc_attr( $id ); ?>" data-dismiss="alert" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+						</button>
+						<?php if ( ! empty( $data['header'] ) ) : ?>
+						<span class="alert-level">
+	
+							<?php if ( ! empty( $data['icon'] ) ) : ?>
+								<span class="ca-gov-icon-<?php print esc_attr( $data['icon'] ); ?>" aria-hidden="true"></span>
+								<?php
+							endif;
+							print esc_html( $data['header'] );
 							?>
-						<a href="<?php print esc_url( $caweb_url ); ?>" class="alert-link btn btn-default btn-xs"<?php print esc_attr( $caweb_target ); ?>><?php print esc_attr( $caweb_text ); ?></a>
+						</span>
 						<?php endif; ?>
+						<span class="alert-text"><?php print wp_kses( wp_unslash( $data['message'] ), caweb_allowed_html( ) ); ?></span>
+							<?php
+							if (  ! empty( $customizer_preview ) || $read_more )  :
+								$read_more_url    = $data['url'];
+								$read_more_target = ! empty( $data['target'] ) ? sprintf( ' target="%1$s"', $data['target'] ) : '';
+								$read_more_text   = ! empty( $data['text'] ) ? $data['text'] : '';
+								?>
+							<a href="<?php print esc_url( $read_more_url ); ?>" class="alert-link btn btn-default btn-xs<?php print ! $read_more ? ' hidden' : ''?>"<?php print esc_attr( $read_more_target ); ?>><?php print esc_attr( $read_more_text ); ?></a>
+							<?php endif; ?>
+					</div>
 				</div>
-			</div>
-			<?php
+				<?php
+			}
 		}
 	}
 }
